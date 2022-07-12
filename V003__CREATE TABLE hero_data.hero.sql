4ac945dd0c21e3482f0121b3d8e41633138cdf8f
Hemanth Katkoju <hemanth.katkoju@wisseninfotech.com>
2022-07-12 17:20:03 +0530

    sql insertion

---
+++ V003__CREATE TABLE hero_data.hero.sql
@@ -0,0 +1,14 @@
+CREATE TABLE hero_data.hero
+(
+    id BIGSERIAL NOT NULL,
+    name VARCHAR(250) NOT NULL,
+    description TEXT NOT NULL,
+    debut_year INT NOT NULL,
+    appearances INT NOT NULL,
+    special_powers INT NOT NULL,
+    cunning INT NOT NULL,
+    strength INT NOT NULL,
+    technology INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+)
