df932f88a201a41cafce3eb102b859e7d652b8f1
hemanthkatkozu <97142087+hemanthkatkozu@users.noreply.github.com>
2022-07-13 10:13:42 +0530

    Delete V1_3__Add_Destroyer_hero.sql

--- V1_3__Add_Destroyer_hero.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO hero_data.hero
-    (name, description, debut_year, appearances, special_powers, cunning, strength, technology, created_at, updated_at)
-VALUES
-    ('Destroyer', 'Created by Odin, locked in temple, brought to life by Loki', 1965, 137, 15, 1, 19, 80, now(), now());
