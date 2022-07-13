cb4e5a5121ba5069b33eb0d2db783b80051cafed
hemanthkatkozu <97142087+hemanthkatkozu@users.noreply.github.com>
2022-07-13 11:25:49 +0530

    Update V005__CREATE TABLE user.sql

--- V005__CREATE TABLE user.sql
+++ V005__CREATE TABLE user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user(
-    id BIGSERIAL NOT NULL,
+    id integer NOT NULL,
     first_name VARCHAR(250) NOT NULL,
     last_name VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL,
